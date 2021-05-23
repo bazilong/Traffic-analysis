d719311210371f3993582462acfd846532304460
bazilong <1356571732@qq.com>
2021-05-23 23:25:26 +0800

    2021 5 23

--- js/my.js
+++ js/my.js
@@ -1,17 +1,24 @@
 var width = 800, height = 700;
 var svg = d3.select("#my_svg").attr("width", width).attr("height", height);
-var geocoder;
-AMap.service('AMap.Geocoder', function () {
-  geocoder = new AMap.Geocoder({
-    city: '海南省'
-  })
-})
+let data;
 function getPositionName(lng, lat) {
-  geocoder.getAddress([lng, lat], function (status, result) {
-    if (status === 'complete' && result.info === 'OK') {
-      // result为对应的地理位置详细信息
-      return (result)
-    }
+  AMap.service('AMap.Geocoder', function () {
+    let geocoder = new AMap.Geocoder({
+      city: '海南省'
+    })
+    geocoder.getAddress([lng, lat], function (status, result) {
+      console.log(1)
+      console.log(result)
+      console.log(result.regeocode.addressComponent.street)
+      let street = result.regeocode.addressComponent.street
+      let township = result.regeocode.addressComponent.township
+      let address = result.regeocode.formattedAddress
+      console.log(address.match(/街道/g))
+      if (status === 'complete' && result.info === 'OK') {
+        // result为对应的地理位置详细信息
+        return (result)
+      }
+    })
   })
 }
 function interpolateData(nations, year, month, day) {
@@ -22,6 +29,12 @@ function interpolateData(nations, year, month, day) {
 }
 function deleteFront0(a) {
   return a.replace(/\b(0+)/gi, "")
+}
+function interpolateStrat(nations, lng, lat) {
+  // filter data in that year
+  return nations.filter(function (a) {
+    return a.starting_lat == lat && a.starting_lng == lng;
+  })
 }
 
 var projection = d3.geoMercator()
@@ -56,54 +69,74 @@ d3.json("./data/haikou-map.json").then(function (res) {
       d3.select(this)
         .attr('fill', 'yellow')
     })
-
-  // svg.append("path")
-  //   .attr("class", "state-borders")
-  //   .attr("d", path(topojson.mesh(us, us, function (a, b) { return a !== b; })));
-
-  // a = { "value": [{ "BusStopCode": "01012", "RoadName": "Victoria St", "Description": "Hotel Grand Pacific", "Latitude": 29.160752671000068, "Longitude": 113.56942793100006 }] }
-  // svg.selectAll("circle")
-  //   .data(a.value).enter().append("circle")
-  //   .attr('stroke', 'red')
-  //   .attr('fill', 'green')
-  //   .attr("d", path)
-  //   .attr("r", 2)
-  //   .attr("transform", function (d) {
-  //     return "translate(" + projection([
-  //       d.Longitude,
-  //       d.Latitude
-  //     ]) + ")";
-  //   })
-  //   .append('title')
-  //   .text(function (d) {
-  //     a = d
-  //     return d.BusStopCode
-  //   });
 });
 //秀英区 460105 龙华区460106 琼山区460107 美兰区 460108
 d3.csv('./data/dwv_order_make_haikou_1_min.csv', function (csvdata) {
   return csvdata;
-}).then(function (data) {
+}).then(function (res) {
+  data = res
   console.log(data)
-  var location = svg.selectAll("location")
+  svg.selectAll("location")
     .data(interpolateData(data, 2017, 5, 17))
     .enter()
     .append("g")
     //.attr("class","location")
     .attr("transform", function (d) {
       return "translate(" + projection([
-        d.dest_lng,
-        d.dest_lat
+        d.starting_lng,
+        d.starting_lat
       ]) + ")";
-    }).append("circle")
+    })
+    .append("circle")
     .attr("r", 5) //半径
     .style("fill", function (d, i) {
       if (d.name == "福州总部")
         return "red";
-
       return "yellow";
-
+    })
+    .on("click", function (d) {
+      getDest(d.starting_lng, d.starting_lat)
+      console.log(getPositionName(d.starting_lng, d.starting_lat))
     })
 })
 
-getPositionName(110.3487, 20.0159)
+
+let lineGenerator = d3.line()
+  .x(function (d) {
+    return d[0]
+  })
+  .y(function (d) {
+    return d[1];
+  });
+
+function getDest(lng, lat) {
+  let paths = interpolateStrat(interpolateData(data, 2017, 5, 17), lng, lat)
+  for (let i = 0; i < paths.length; i++) {
+    console.log(paths[i])
+    svg.selectAll("path")
+      .enter()
+      .append('path')
+      .attr('stroke', '#ccc')
+      .attr('stroke-width', '2')
+      .attr('fill', 'none')
+      .attr('d', lineGenerator([projection(paths[i].starting_lng, paths[i].starting_lat), projection(paths[i].dest_lng, paths[i].dest_lat)]));
+  }
+  // svg.selectAll("location")
+  //   .data(interpolateStrat(interpolateData(data, 2017, 5, 17), lng, lat))
+  //   .enter()
+  //   .append('path')
+  //       .attr('stroke', '#ccc')
+  //       .attr('stroke-width', '2')
+  //       .attr('fill', 'none')
+  //       .attr('d', lineGenerator(data));
+
+  //   .attr("transform", function (d) {
+  //     return "translate(" + projection([
+  //       d.dest_lng,
+  //       d.dest_lat
+  //     ]) + ")";
+  //   })
+  //   .append("circle")
+  //   .attr("r", 5) //半径
+  //   .style("fill", "red");
+}
